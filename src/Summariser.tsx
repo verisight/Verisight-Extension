e83a41d1dae5a1d8edf89761ffc1fd4a4aef8022
Sudesh Tharoosh <zoimbu73@gmail.com>
2024-01-30 20:15:55 +0530

    summarise

--- src/Summariser.tsx
+++ src/Summariser.tsx
@@ -1,10 +1,77 @@
+import { useState } from 'react';
+import brief from './assets/Brief.jpg';
+import detective from './assets/Detective.jpg';
+import plus from './assets/Plus Math.jpg';
+
+import './MainPage.css';
+
+
+
 function Summariser() {
-  return (
-    <div>
-      <h1>Summariser Page</h1>
-      {/* Add your Summariser content here */}
-    </div>
-  );
+
+    const [showSummariser, setShowSummariser] = useState(false); // State to control the visibility of Summariser
+    const handleSummariserClick = () => {
+        setShowSummariser(true); // Show Summariser component when the button is clicked
+      };
+    
+      // Render the Summariser component if showSummariser is true
+      if (showSummariser) {
+        return <Summariser />;
+      }
+
+    return (
+        <div className="extension-summariser">
+          <div className="div">
+            <div className="overlap">
+              <div className="text-wrapper">Article Headline</div>
+            </div>
+            <div className="text-wrapper-2">LOGO</div>
+            <div className="overlap-group">
+              <p className="lorem-ipsum-dolor">
+                Lorem ipsum dolor sit amet consectetur. Semper enim a ipsum elit eu at. Eu urna laoreet faucibus nunc cras
+                sagittis eu. Urna sed aliquam purus lacus eget facilisi feugiat commodo dolor. Quam aliquam tempor aliquam
+                sollicitudin lectus. Semper tortor non mauris vulputate.
+                <br />
+                Proin pellentesque vel dignissim urna et commodo consectetur feugiat. Dapibus porttitor turpis tristique
+                cras neque cras fusce eu. Amet tortor ut fusce sodales vitae praesent molestie amet tincidunt. Sodales
+                faucibus morbi sodales vulputate viverra nulla in volutpat. Dis mi ac aenean purus dolor. Vel risus turpis
+                sit gravida in.
+              </p>
+              <img className="vector" alt="Vector" src="vector.svg" />
+            </div>
+            <div className="div-wrapper">
+              <div className="text-wrapper-3">Get Summary</div>
+            </div>
+            {/* <div className="menu">
+              <div className="overlap-group-2">
+                <div className="rectangle" />
+                <div className="ellipse" />
+                <img className="plus-math" alt="Plus math" src="plus-math.png" />
+                <div className="text-wrapper-4">Summariser</div>
+                <div className="text-wrapper-5">Cross-checking</div>
+                <img className="brief" alt="Brief" src="brief.png" />
+                <img className="detective" alt="Detective" src="detective.png" />
+              </div>
+            </div> */}
+            <div className="box">
+                <div className="menu">
+                    <div className="overlap-group">
+                        <button className="rectangle summariser" onClick={handleSummariserClick}></button>
+                        <button className="rectangle cross-checking"></button>
+                        <button className="ellipse"></button>
+                        <img src={plus} className="plus-math" alt="Plus math" />
+                        <div className="text-wrapper">Summariser</div>
+                        <div className="div">Cross-checking</div>
+                        <img src={brief} className="brief" alt="Brief" />
+                        <img src={detective} className="detective" alt="Detective" />
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    );
 }
 
 export default Summariser;
+
+
