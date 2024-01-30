79f1991cfa1b8c7d5a6d94d415906578ab2af3ba
PPT1001 <pragash1001@gmail.com>
2024-01-30 21:11:51 +0530

    App Changes

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function GetStartedPage({ onGetStartedClick }: { onGetStartedClick: () => void }
   return (
     <div>
       <img src={blueTickLogo} className="bigLogo" alt="Blue Tick logo" />
-      <h2>Don’t trust whatever you see</h2>
+      <h2>Don't trust whatever you see</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel sem vel felis egestas
         lobortis.
