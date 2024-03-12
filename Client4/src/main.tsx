610c573e2941c51a089e880cd15b8dbc0e1e5ece
PPT1001 <pragash1001@gmail.com>
2024-03-12 09:40:14 +0530

    Update extension framing

--- Client4/src/main.tsx
+++ Client4/src/main.tsx
@@ -12,43 +12,44 @@
 //   </React.StrictMode>,
 // )
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { ThemeProvider } from './components/ui/theme-provider.tsx'
-import { RouterProvider, createHashRouter } from 'react-router-dom'
-import Onboard from './onboard/Onboard.tsx'
-import Authentication from './auth/Authentication.tsx'
-import Home from './home/Home.tsx'
-import Report from './report/Report.tsx'
-import AppContext from './GlobalContext.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "./components/ui/theme-provider.tsx";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import Onboard from "./onboard/Onboard.tsx";
+import Authentication from "./auth/Authentication.tsx";
+import Home from "./home/Home.tsx";
+import Report from "./report/Report.tsx";
+import AppContext from "./GlobalContext.tsx";
 
-const router = createHashRouter([{
-  path: "/",
-  element: <Onboard />,
-},
-{
-  path: "/auth",
-  element: <Authentication />
-},
-{
-  path: "/home",
-  element: <Home />
-},
-{
-  path: "/report",
-  element: <Report />
-}
-])
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Onboard />,
+  },
+  {
+    path: "/auth",
+    element: <Authentication />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/report",
+    element: <Report />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className='w-96 h-96'>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AppContext>
-        <RouterProvider router={router} />
-      </AppContext>
-    </ThemeProvider>
+    <div className="w-[400px] h-[585px]">
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <AppContext>
+          <RouterProvider router={router} />
+        </AppContext>
+      </ThemeProvider>
     </div>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- Client4/src/report/Summary.tsx
+++ Client4/src/report/Summary.tsx
@@ -1,52 +1,65 @@
-import { useGlobalContext } from '@/GlobalContext';
-import { Button } from '@/components/ui/button';
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
-import { TabsContent } from '@/components/ui/tabs';
-import { Textarea } from '@/components/ui/textarea';
-import { useState } from 'react';
+import { useGlobalContext } from "@/GlobalContext";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { TabsContent } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 const Summary = () => {
-  const {article} = useGlobalContext();
+  const { article } = useGlobalContext();
 
-  const [summary, setSummary] = useState('');
+  const [summary, setSummary] = useState("");
 
   const getSummary = async () => {
     // Fetch summary from server
-    const response = await fetch('http://localhost:3000/summary', {
-      method: 'POST',
+    const response = await fetch("http://localhost:3000/summary", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(article)
+      body: JSON.stringify(article),
     });
     const data = await response.text();
     return data;
-  }
+  };
 
   const handleGetSummary = async () => {
     const summary = await getSummary();
     setSummary(summary);
-  }
+  };
 
+  return (
+    <TabsContent value="summary">
+      <Card>
+        <CardHeader>
+          <CardTitle>Article Summary</CardTitle>
+          <CardDescription>Summarize the article using AI</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Textarea
+              id="summary"
+              readOnly
+              value={summary}
+              className="resize-none h-72"
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" onClick={handleGetSummary}>
+            Get Summary
+          </Button>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+  );
+};
 
-    return (<TabsContent value="summary">
-    <Card>
-      <CardHeader>
-        <CardTitle>Article Summary</CardTitle>
-        <CardDescription>
-          Summarize the article using AI
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-2">
-      <div className="space-y-1">
-          <Textarea id="summary" readOnly value={summary} className="resize-none h-72" />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full" onClick={handleGetSummary}>Get Summary</Button>
-      </CardFooter>
-    </Card>
-  </TabsContent>);
-}
-
-export default Summary
+export default Summary;
