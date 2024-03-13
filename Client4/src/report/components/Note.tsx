695d79260dd6c4a0d1da2216f4fb62d120df67c5
Mohamed Aashif <maashif011@gmail.com>
2024-03-13 19:19:37 +0530

    Add upvote functionality to Note component

--- Client4/src/report/components/Note.tsx
+++ Client4/src/report/components/Note.tsx
@@ -1,12 +1,59 @@
+import { useGlobalContext } from "@/GlobalContext";
 import { Card } from "@/components/ui/card";
 import { Toggle } from "@/components/ui/toggle";
 import { ThumbsUp } from "lucide-react";
+import { useEffect, useState } from "react";
 
-const Upvote = () => {
+const Upvote = (props: { id: string }) => {
+    const { id } = props;
+    const {user} = useGlobalContext();
+
+    const [upvoted, setUpvoted] = useState(false);
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/notes/isUpvoted", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                noteId: id,
+                userId: user.userId, 
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setUpvoted(data);
+            });
+    }, [])
+    
     return (
-        <Toggle>
-            <ThumbsUp className="h-4 w-4"/>
-        </Toggle>
+<Toggle 
+    pressed={upvoted} 
+    onPressedChange={() => { 
+        fetch("http://localhost:3000/notes/upvote", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                _id: id,
+                userId: user.userId, 
+            }),
+        })
+        .then(() => fetch("http://localhost:3000/notes/isUpvoted", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                noteId: id,
+                userId: user.userId,
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setUpvoted(data);
+        }));
+    }}
+>
+    <ThumbsUp className="h-4 w-4"/>
+</Toggle>
+
 
     )
 }
@@ -17,7 +64,7 @@ export const Note = (props: { content: string, id: string }) => {
         <Card className="mb-5 w-full" key={id}>
             <div className="p-4 flex justify-between items-center">
                 <p className="text-sm">{content}</p>
-                <Upvote />
+                <Upvote id={id}/>
             </div>
         </Card>
     );
