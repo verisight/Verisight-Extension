80605d328f8a4bd71ee49c2120fb57a7e7d7d33d
Mohamed Aashif <maashif011@gmail.com>
2024-03-13 15:38:19 +0530

    Add note fetching functionality to UserNoteView component

--- Client4/src/report/UserNoteView.tsx
+++ Client4/src/report/UserNoteView.tsx
@@ -3,8 +3,30 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from 
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Note } from "./components/Note"
 import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react";
+import { useGlobalContext } from "@/GlobalContext";
+
+
 
 const UserNoteView = () => {
+
+    const { article } = useGlobalContext();
+    
+
+    const [notes, setNotes] = useState<any>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/notes/all", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ articleLink: article.link }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setNotes(data);
+            });
+    }, []);
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -14,7 +36,11 @@ const UserNoteView = () => {
                 <DialogHeader className="m-0">
                     <DialogTitle>User Notes</DialogTitle>
                     <ScrollArea className="h-72 w-full p-5">
-                        <Note content="This is a note" id="asd" />
+                        {notes.map((note: any) => {
+                            return (
+                                <Note content={note.noteContent} id={note._id} />
+                            )
+                        })}
                     </ScrollArea>
                 </DialogHeader>
             </DialogContent>
