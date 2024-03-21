e88429e74f689900737c4f66ef03beabc0673ca0
PPT1001 <pragash1001@gmail.com>
2024-03-21 10:58:58 +0530

    Updated to differentiate between upvote and delete

--- Client4/src/report/components/Note.tsx
+++ Client4/src/report/components/Note.tsx
@@ -1,71 +1,96 @@
 import { useGlobalContext } from "@/GlobalContext";
 import { Card } from "@/components/ui/card";
 import { Toggle } from "@/components/ui/toggle";
-import { ThumbsUp } from "lucide-react";
+import { ThumbsUp, Trash2 } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const Upvote = (props: { id: string }) => {
-    const { id } = props;
-    const {user} = useGlobalContext();
+  const { id } = props;
+  const { user } = useGlobalContext();
 
-    const [upvoted, setUpvoted] = useState(false);
-    
-    useEffect(() => {
-        fetch("http://localhost:3000/notes/isUpvoted", {
+  const [upvoted, setUpvoted] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/notes/isUpvoted", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        noteId: id,
+        userId: user.userId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUpvoted(data);
+      });
+  }, []);
+
+  return (
+    <Toggle
+      pressed={upvoted}
+      onPressedChange={() => {
+        fetch("http://localhost:3000/notes/upvote", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            _id: id,
+            userId: user.userId,
+          }),
+        }).then(() =>
+          fetch("http://localhost:3000/notes/isUpvoted", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                noteId: id,
-                userId: user.userId, 
+              noteId: id,
+              userId: user.userId,
             }),
-        })
+          })
             .then((response) => response.json())
             .then((data) => {
-                setUpvoted(data);
-            });
-    }, [])
-    
-    return (
-<Toggle 
-    pressed={upvoted} 
-    onPressedChange={() => { 
-        fetch("http://localhost:3000/notes/upvote", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                _id: id,
-                userId: user.userId, 
-            }),
-        })
-        .then(() => fetch("http://localhost:3000/notes/isUpvoted", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                noteId: id,
-                userId: user.userId,
+              setUpvoted(data);
             })
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            setUpvoted(data);
-        }));
-    }}
->
-    <ThumbsUp className="h-4 w-4"/>
-</Toggle>
+        );
+      }}
+    >
+      <ThumbsUp className="h-4 w-4" />
+    </Toggle>
+  );
+};
 
+const Delete = (props: { id: string }) => {
+  const { id } = props;
+  const { user } = useGlobalContext();
 
-    )
-}
+  return (
+    <button
+      onClick={() => {
+        fetch("http://localhost:3000/notes/delete", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            _id: id,
+            userId: user.userId,
+          }),
+        });
+      }}
+    >
+      <Trash2 className="h-4 w-4" />
+    </button>
+  );
+};
 
-export const Note = (props: { content: string, id: string }) => {
-    const { content, id } = props;
-    return (
-        <Card className="mb-5 w-full" key={id}>
-            <div className="p-4 flex justify-between items-center">
-                <p className="text-sm">{content}</p>
-                <Upvote id={id}/>
-            </div>
-        </Card>
-    );
-}
+export const Note = (props: {
+  content: string;
+  id: string;
+  upVote: boolean;
+}) => {
+  const { content, id, upVote } = props;
+  return (
+    <Card className="mb-5 w-full" key={id}>
+      <div className="p-4 flex justify-between items-center">
+        <p className="text-sm">{content}</p>
+        {upVote === true ? <Upvote id={id} /> : <Delete id={id} />}
+      </div>
+    </Card>
+  );
+};
